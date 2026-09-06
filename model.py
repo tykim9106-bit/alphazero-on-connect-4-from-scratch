fb2e3e051e7206ae7e5c5660324a8ad893e62ff3
tykim9106-bit <tykim9106@gmail.com>
2026-09-06 10:24:01 +0900

    Step 14: step_env

--- model.py
+++ model.py
@@ -161,8 +161,14 @@ def is_terminal(board):
 def other_player(player):
     return 3-player
 
-# Step 14 - step_env (not yet solved)
-# TODO: implement
+# Step 14 - step_env
+def step_env(board, column, player):
+    # TODO: drop piece for player, then return (new_board, done, winner, next_player).
+    new_board = drop_piece(board, column, player)
+    done, winner = is_terminal(new_board)
+    next_player = other_player(player)
+
+    return new_board, done, winner, next_player
 
 # Step 15 - encode_board (not yet solved)
 # TODO: implement
